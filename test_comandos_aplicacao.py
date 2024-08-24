56f6044cac34ff41b1d7c508b0ba64a558dd97cd
Ricardo Anjos <83664969+ricardojaj@users.noreply.github.com>
2024-08-24 20:59:49 -0300

    Update test_comandos_aplicacao.py

--- test_comandos_aplicacao.py
+++ test_comandos_aplicacao.py
@@ -3,12 +3,12 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-browser = webdriver.Chrome();
+browser = webdriver.Chrome()
 
-browser.get("https://www.saucedemo.com/");
+browser.get("https://www.saucedemo.com/")
 
-print("Titulo pagina: ", browser.title);
+print("Titulo pagina: ", browser.title)
 
-print("URL: ", browser.current_url);
+print("URL: ", browser.current_url)
 
-print("Codigo Fonte: ", browser.page_source);
+print("Codigo Fonte: ", browser.page_source)
