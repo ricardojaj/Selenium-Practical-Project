d86feefe3a39974af49d9d2998011ed757235187
Ricardo Anjos <83664969+ricardojaj@users.noreply.github.com>
2024-08-24 20:57:15 -0300

    Update test_comandos_interacao_com_elementos.py

--- test_comandos_interacao_com_elementos.py
+++ test_comandos_interacao_com_elementos.py
@@ -2,28 +2,28 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-browser = webdriver.Chrome();
-browser.get("https://www.saucedemo.com/");
+browser = webdriver.Chrome()
+browser.get("https://www.saucedemo.com/")
 browser.maximize_window()
 
 # find_element( )
-username = browser.find_element(By.ID, "user-name");
-password = browser.find_element(By.ID, "password");
+username = browser.find_element(By.ID, "user-name")
+password = browser.find_element(By.ID, "password")
 btnLogin = browser.find_element(By.ID, "login-button")
 
 
 # send_keys
-username.send_keys("standard_user");
-password.send_keys("secret_sauce");
+username.send_keys("standard_user")
+password.send_keys("secret_sauce")
 
 #click( )
 btnLogin.click()
 time.sleep(5)
 
 #text
-products_title = browser.find_element(By.XPATH, "//span[@class='title']");
-print(products_title.text);
-assert products_title.text == "Products";
+products_title = browser.find_element(By.XPATH, "//span[@class='title']")
+print(products_title.text)
+assert products_title.text == "Products"
 
 
 #get_attribute()
