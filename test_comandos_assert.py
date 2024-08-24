c2f8d51e4fa8ff0a21b6594313d927ab2af49b3f
Ricardo Anjos <83664969+ricardojaj@users.noreply.github.com>
2024-08-24 20:59:25 -0300

    Update test_comandos_assert.py

--- test_comandos_assert.py
+++ test_comandos_assert.py
@@ -3,19 +3,19 @@ assert True
 
 
 # assert numbers
-#num_esperado = 1;
-#num_obtido = 2;
+#num_esperado = 1
+#num_obtido = 2
 #assert num_esperado == num_obtido, f"Esperado {num_esperado}. Atual {num_obtido}"
 
 
 #assert text
-#titulo = "Selenium Web Driver";
+#titulo = "Selenium Web Driver"
 #tituloPrototipo = "Selenium Web Driver"
 #assert titulo == tituloPrototipo, f"titulo no site {titulo}. Titulo no prototipo {tituloPrototipo}"
 
 
 #assert text in string
-titulo = "seleniuuuuuuuuuuu";
+titulo = "seleniuuuuuuuuuuu"
 tituloPrototipo = "Selenium Web Driver"
 
 assert titulo in tituloPrototipo, f"titulo no site '{titulo}'. Titulo no prototipo '{tituloPrototipo}'"
