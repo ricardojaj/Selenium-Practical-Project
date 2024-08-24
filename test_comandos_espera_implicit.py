39bc14ad73d307cd880d92c40a52ab895388bce5
Ricardo Anjos <83664969+ricardojaj@users.noreply.github.com>
2024-08-24 20:57:36 -0300

    Update test_comandos_espera_implicit.py

--- test_comandos_espera_implicit.py
+++ test_comandos_espera_implicit.py
@@ -2,10 +2,10 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-browser = webdriver.Chrome();
+browser = webdriver.Chrome()
 browser.implicitly_wait(12)
 
-browser.get("https://chercher.tech/practice/implicit-wait-example");
+browser.get("https://chercher.tech/practice/implicit-wait-example")
 browser.maximize_window()
 
 checkbox = browser.find_element(By.XPATH, "//input[@type='checkbox']")
